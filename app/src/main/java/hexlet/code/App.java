8e3ab913752c75185980a3432a2864d5d9e032df
toxigen <mityashin.dima@gmail.com>
2023-07-16 15:52:13 +1000

    add checkstyle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,26 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greetingsCli();
+        System.out.print("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "2 - Even\n"
+                + "0 - Exit\n"
+                + "Your choice: ");
+        Scanner sc = new Scanner(System.in);
+        int select = sc.nextInt();
+        switch (select) {
+            case 1:
+                Cli.greetingsCli();
+                break;
+            case 2:
+                Even.playEven();
+            case 0:
+                break;
+            default:
+        }
+        sc.close();
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,11 +2,16 @@ package hexlet.code;
 
 import java.util.Scanner;
 public class Cli {
+    private static String name;
     public static void greetingsCli() {
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        System.out.println("Hello, " + name);
-        sc.close();
+        name = sc.next();
+        System.out.println("Hello, " + name + "!");
+    }
+
+    public static String getName() {
+        return name;
     }
 }
