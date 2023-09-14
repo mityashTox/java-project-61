b609d363314f56003c295dbedde3094368eb454a
mityashTox <63090077+mityashTox@users.noreply.github.com>
2023-09-14 21:05:44 +1000

    upgrade Calc

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -8,8 +8,10 @@ public final class Calc implements Game {
     private static final int ARRAY_SIZE_1 = 3;
     private static final int ARRAY_SIZE_2 = 2;
     private static final String GAME_RULE = "What is the result of the expression?";
-    private static final int SWITCH_PLUS = 2;
-    private static final int SWITCH_MINUS = 6;
+    private static final int SWITCH_PLUS = 1;
+    private static final int SWITCH_MINUS = 2;
+    private static final int SWITCH_MULTIPLICATION = 3;
+    private static final int NON_RESULT = 0;
     private static final int INCREASE_THE_MANTISSA = 10;
 
 
@@ -25,28 +27,52 @@ public final class Calc implements Game {
 
     @Override
     public String[][] askQuestions() {
-        String[][] stackQuestion = new  String[ARRAY_SIZE_1][ARRAY_SIZE_2];
+        String[][] stackQuestion = new String[ARRAY_SIZE_1][ARRAY_SIZE_2];
         for (int i = 0; i < stackQuestion.length; i++) {
             int randomValue1 = Utils.makeRandomValue();
             int randomValue2 = Utils.makeRandomValue();
-            int answer;
-            String expression;
-            if (Math.random() * INCREASE_THE_MANTISSA < 3) {
-                answer = randomValue1 + randomValue2;
-                expression = "+";
-            } else if (Math.random() * INCREASE_THE_MANTISSA >= SWITCH_PLUS && Math.random()
-                    * INCREASE_THE_MANTISSA < SWITCH_MINUS) {
-                answer = randomValue1 - randomValue2;
-                expression = "-";
-            } else {
-                answer = randomValue1 * randomValue2;
-                expression = "*";
-            }
+            char expression = makeSign();
+            int answer = calculate(expression, randomValue1, randomValue2);
+//            if (Math.random() * INCREASE_THE_MANTISSA < ARRAY_SIZE_1) {
+//                answer = randomValue1 + randomValue2;
+//                expression = "+";
+//            } else if (Math.random() * INCREASE_THE_MANTISSA >= SWITCH_PLUS && Math.random()
+//                    * INCREASE_THE_MANTISSA < SWITCH_MINUS) {
+//                answer = randomValue1 - randomValue2;
+//                expression = "-";
+//            } else {
+//                answer = randomValue1 * randomValue2;
+//                expression = "*";
+//            }
             stackQuestion[i][0] = randomValue1 + " " + expression + " " + randomValue2;
             stackQuestion[i][1] = Integer.toString(answer);
         }
         return stackQuestion;
     }
 
+    private static char makeSign() {
+        switch ((int) (Math.random() * (SWITCH_MULTIPLICATION) + SWITCH_PLUS)) {
+            case SWITCH_PLUS:
+                return '+';
+            case SWITCH_MINUS:
+                return '-';
+            case SWITCH_MULTIPLICATION:
+                return '*';
+            default:
+                return NON_RESULT;
+        }
+    }
 
+    private static int calculate(char operator, int number1, int number2) {
+        switch (operator) {
+            case '+':
+                return number1 + number2;
+            case '-':
+                return number1 - number2;
+            case '*':
+                return number1 * number2;
+            default:
+                return NON_RESULT;
+        }
+    }
 }
