2241419d707d3cc36492a98e7f3507a1f05d5ef8
mityashTox <63090077+mityashTox@users.noreply.github.com>
2023-11-30 13:29:31 +1000

    upgrade random expression

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -8,10 +8,8 @@ public final class Calc implements Game {
     private static final int ARRAY_SIZE_1 = 3;
     private static final int ARRAY_SIZE_2 = 2;
     private static final String GAME_RULE = "What is the result of the expression?";
-    private static final int SWITCH_PLUS = 1;
-    private static final int SWITCH_MINUS = 2;
-    private static final int SWITCH_MULTIPLICATION = 3;
     private static final int NON_RESULT = 0;
+    private  final char[] operators = {'+', '-', '*'};
 
     @Override
     public void printGameRule() {
@@ -29,25 +27,12 @@ public final class Calc implements Game {
         for (int i = 0; i < stackQuestion.length; i++) {
             int randomValue1 = Utils.makeRandomValue();
             int randomValue2 = Utils.makeRandomValue();
-            char expression = makeSign();
-            int answer = calculate(expression, randomValue1, randomValue2);
-            stackQuestion[i][0] = randomValue1 + " " + expression + " " + randomValue2;
+            int expression = (int)Math.floor(Math.random() * operators.length);
+            int answer = calculate(operators[expression], randomValue1, randomValue2);
+            stackQuestion[i][0] = randomValue1 + " " + operators[expression] + " " + randomValue2;
             stackQuestion[i][1] = Integer.toString(answer);
         }
         return stackQuestion;
-    }
-
-    private static char makeSign() {
-        switch ((int) (Math.random() * (SWITCH_MULTIPLICATION) + SWITCH_PLUS)) {
-            case SWITCH_PLUS:
-                return '+';
-            case SWITCH_MINUS:
-                return '-';
-            case SWITCH_MULTIPLICATION:
-                return '*';
-            default:
-                return NON_RESULT;
-        }
     }
 
     private static int calculate(char operator, int number1, int number2) {
